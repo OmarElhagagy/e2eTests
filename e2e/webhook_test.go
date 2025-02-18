36f5d2a8125f0e09bbb5370e9a13e1436c7a5346
OmarElhagagy <oadeell21@gmail.com>
2025-02-18 22:29:37 +0200

    made new file in e2e to test webhooks will continue from verifying the app is mutated

---
+++ e2e/webhook_test.go
@@ -0,0 +1,141 @@
+//nolint:all
+package e2e_test
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	argocdv1alpha1 "github.com/argoproj/argo-cd/v2/pkg/apis/application/v1alpha1"
+	argoprojv1alpha1 "github.com/argoproj/argo-cd/v2/pkg/apis/application/v1alpha1"
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+	"github.com/peak-scale/capsule-argo-addon/api/v1alpha1"
+	capsulev1beta2 "github.com/projectcapsule/capsule/api/v1beta2"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+)
+
+var _ = Describe("Mutating Webhooks", func() {
+	const (
+		testNamespace      = "argocd"
+		defaultTimeout     = time.Second * 10
+		defaultTestTenant  = "test-tenat"
+		defaultAppName     = "test-app"
+		defaultAppSetName  = "test-appset"
+		defaultRepoURL     = "https://github.com/argoproj/argocd-example-apps"
+		defaultPath        = "guestbook"
+		defaultDestination = "in-cluster"
+	)
+
+	var (
+		ctx        context.Context
+		tenant     *capsulev1beta2.Tenant
+		translator *v1alpha1.ArgoTranslator
+	)
+
+	BeforeEach(func() {
+		ctx = context.Background()
+		// test tenat for our test
+		tenant = &capsulev1beta2.Tenant{
+			ObjectMeta: metav1.ObjectMeta{
+				Name:   defaultTestTenant,
+				Labels: e2eLabels("webhook-test"),
+			},
+			Spec: capsulev1beta2.TenantSpec{
+				Owners: capsulev1beta2.OwnerListSpec{
+					{
+						Name: "webhook-test-owner",
+						Kind: "User",
+					},
+				},
+			},
+		}
+		Expect(k8sClient.Create(ctx, tenant)).To(Succeed())
+
+		// Translator for our test
+		translator = &v1alpha1.ArgoTranslator{
+			ObjectMeta: metav1.ObjectMeta{
+				Name:   defaultTestTenant,
+				Labels: e2eLabels("webhook-test"),
+			},
+			Spec: v1alpha1.ArgoTranslatorSpec{
+				Tenant: defaultTestTenant,
+				ArgoCD: v1alpha1.ArgoTranslatorSpec{
+					Destination: defaultDestination,
+					Namespace:   testNamespace,
+					Project:     defaultTestTenant,
+				},
+			},
+		}
+		Expect(k8sClient.Create(ctx, translator)).To(Succeed())
+
+		// ArgoCD project
+		project := &argocdv1alpha1.AppProject{
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      defaultTestTenant,
+				Namespace: testNamespace,
+				Labels:    e2eLabels("webhook-test"),
+			},
+			Spec: argocdv1alpha1.AppProjectSpec{
+				SourceRepos: []string{"*"},
+				Destinations: []argocdv1alpha1.ApplicationDestination{
+					{
+						Server:    "*",
+						Namespace: "*",
+					},
+				},
+			},
+		}
+		Expect(k8sClient.Create(ctx, project)).To(Succeed())
+	})
+	AfterEach(func() {
+		selector := e2eSelector("webhook-test")
+		Expect(CleanTenants(selector)).To(Succeed())
+		Expect(CleanTranslators(selector)).To(Succeed())
+		Expect(CleanAppProjects(selector, testNamespace)).To(Succeed())
+		Expect(cleanApplications(selector, testNamespace)).To(Succeed())
+		Expect(cleanApplicationSets(selector, testNamespace)).To(Succeed())
+	})
+
+	Context("Application Mutating Webhook", func() {
+		When("creating an application with webhook enabled", func() {
+			It("should mutate the application correctly", func() {
+				// update ArgoAddon to enable webhook for application
+				Expect(k8sClient.Get(ctx, client.ObjectKey{Name: "default"}, argoAddon)).To(Succeed())
+				argoAddon.Spec.ApplicationWebhook = true
+				Expect(k8sClient.Update(ctx, argoAddon)).To(Succeed())
+
+				// create an application
+				app := &argocdv1alpha1.Application{
+					ObjectMeta: metav1.ObjectMeta{
+						Name:      defaultAppName,
+						Namespace: testNamespace,
+						Labels: map[string]string{
+							"capsule.clastix.io/tenant": defaultTestTenant,
+							e2eLabel:                    "true",
+							suiteLabel:                  "webhook-test",
+						},
+					},
+					Spec: argocdv1alpha1.ApplicationSpec{
+						Project: "default", // this should be mutated to tenant name
+						Source: &argocdv1alpha1.ApplicationSource{
+							RepoURL:        defaultRepoURL,
+							Path:           defaultPath,
+							TargetRevision: "HEAD",
+						},
+						Destination: argocdv1alpha1.ApplicationDestination{
+							Server:    "https://kubernetes.default.svc",
+							Namespace: "default",
+						},
+					},
+				}
+
+				Expect(k8sClient.Create(ctx, app)).To(Succeed())
+
+				// verify that the app is mutated
+			})
+		})
+	})
+
+})
